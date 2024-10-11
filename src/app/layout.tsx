4bf0899a4d647fae4853dba66facb16064803c5e
fbonilla02 <franciscobonillac@gmail.com>
2024-10-11 18:27:07 -0500

    move page and layout to main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { dmSans, roboto } from "@/config/fonts";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -20,16 +9,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${roboto.variable} ${dmSans.variable}`}>
+      <body>{children}</body>
     </html>
   );
 }
